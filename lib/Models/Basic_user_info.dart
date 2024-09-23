0326a665d350782f85cf0b950d99d769008ea023
84796578ran <84796578foru.com>
2024-09-23 11:20:09 +0700

    login page

--- lib/Models/Basic_user_info.dart
+++ lib/Models/Basic_user_info.dart
@@ -1,10 +1,28 @@
-class BasicUserInfo{
-  final String? username;
+class BasicUserInfo {
+  BasicUserInfo({
+    this.userName,
+    this.phone,
+    required this.address,
+    this.email,
+    this.fullName,
+    this.gender,
+  });
+
+  final String? userName;
   final String? phone;
   final String? address;
   final String? email;
   final String? fullName;
-  BasicUserInfo({
-  
-})
+  final String? gender;
+
+  factory BasicUserInfo.fromJson(Map<String, dynamic> json) {
+    return BasicUserInfo(
+      userName: json['userName'],
+      phone: json['phone'],
+      address: json['address'],
+      email: json['email'],
+      fullName: json['fullName'],
+      gender: json['gender'],
+    );
+  }
 }
