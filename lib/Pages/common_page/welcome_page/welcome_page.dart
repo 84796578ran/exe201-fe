9dc211b2d0626937d1d580094dfc7f2e3528c770
84796578ran <84796578foru.com>
2024-11-23 18:25:00 +0700

    home page

--- lib/Pages/common_page/welcome_page/welcome_page.dart
+++ lib/Pages/common_page/welcome_page/welcome_page.dart
@@ -5,18 +5,32 @@ class WelcomePage extends StatefulWidget {
 
   const WelcomePage({super.key});
 
+
+
   @override
   State<WelcomePage> createState() => _WelcomePageState();
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  final username = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: Column()
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
