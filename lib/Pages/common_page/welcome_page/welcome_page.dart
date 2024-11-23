82db83399e7aa54885f90d4e454fa99a97626f75
84796578ran <84796578foru.com>
2024-11-23 17:14:27 +0700

    welcome-pagedone

---
+++ lib/Pages/common_page/welcome_page/welcome_page.dart
@@ -0,0 +1,24 @@
+  import 'package:flutter/material.dart';
+
+
+class WelcomePage extends StatefulWidget {
+
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Column()
+    );
+  }
+}
+
+
---
+++ lib/Widgets/nav_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold();
+  }
+}
+
